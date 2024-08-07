7135f4d7f0f82b0c117603c9866927675bf4cd1d
smrahr <aharisamira@gmail.com>
2024-08-07 18:55:02 +0330

    feat: add images to game

--- src/components/home/EachGameSection.jsx
+++ src/components/home/EachGameSection.jsx
@@ -1,13 +1,21 @@
+import { Link } from "react-router-dom";
+
 const EachGameSection = ({ game }) => {
   return (
     <div className="w-full rouded-md py-3 relative">
       <div>
         <img src={game.image} alt="gameImage" />
       </div>
-      <div className="absolute top-[50%] left-[50%]">
-        <div className="flex flex-col justify-center items-center">
+      <div className="w-full h-full absolute top-0 left-0">
+        <div className="w-full h-full flex flex-col justify-center items-center ">
           <h2 className="text-[20px] font-bold">{game.title}</h2>
           <p className="text-[20px]">{game.description}</p>
+          <p className="text-[20px]">{game.description}</p>
+          <button className="border-4 border-white rounded-full w-[140px] h-[40px] justify-center items-center mt-[18px]">
+            <Link target="_blank" to="/">
+              <p className="text-[13px]">learn more</p>
+            </Link>
+          </button>
         </div>
       </div>
     </div>
