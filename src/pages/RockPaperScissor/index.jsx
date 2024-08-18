f2db364d4c6d0b8b126be85778af66f2f38d017d
smrahr <aharisamira@gmail.com>
2024-08-18 14:54:11 +0330

    feat: create faq page

--- src/pages/RockPaperScissor/index.jsx
+++ src/pages/RockPaperScissor/index.jsx
@@ -1,5 +1,25 @@
+import RockPaperScissorIntro from "@components/rockPaperScissor/RockPaperScissorIntro";
+import PronsAndCons from "@components/rockPaperScissor/ProsAndCons";
+import Streaks from "@components/rockPaperScissor/Streaks";
+import Boosters from "@components/rockPaperScissor/Boosters";
+
 const RockPaperScissor = () => {
-  return <div>RockPaperScissor</div>;
+  return (
+    <div>
+      <div className="container max-w-[1152px]">
+        <RockPaperScissorIntro />
+      </div>
+      <div className="container max-w-[1152px]">
+        <PronsAndCons />
+      </div>
+      <div className="container max-w-[1152px]">
+        <Streaks />
+      </div>
+      <div className="container max-w-[1152px]">
+        <Boosters />
+      </div>
+    </div>
+  );
 };
 
 export default RockPaperScissor;
