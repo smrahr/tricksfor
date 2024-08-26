bb910615005d62276f7802c3c9f5d803bb1df81c
Hamed Ansarian <hamed.ansarian@hotmail.com>
2024-08-26 19:07:44 +0330

    update FAQ content

--- src/constants/faqData.js
+++ src/constants/faqData.js
@@ -1,44 +1,44 @@
 export const faqData = [
   {
     id: 1,
-    question: "How to start?",
+    question: "How can I play games?",
     answer:
-      "After you sign up, you can either start an FTMO Challenge right away or, as we usually recommend, start with our Free Trial, which is a free version of our FTMO Challenge intended for practicing purposes. In the Free Trial, you will also have the possibility to test some of our Trading Applications and you will also receive a shortened analysis version of your performance in the Free Trial. You can retake your Free Trial as many times as you want to make sure you are fully prepared for an FTMO Challenge",
+      "Start by logging into the app using your Web3 wallet, then Navigate through the app to find the game you want to play. Examine the game statistics to make an informed decision and Predict the winner by adding cryptocurrencies to your prediction.",
     to: "/How-to-start?",
   },
   {
     id: 2,
-    question: "Who can join FTMO?",
+    question: "What are statistics of the game?",
     answer:
-      "No specific qualifications are required. All clients must be over the age of 18. Whether you are a beginner or already know your way around trading and are interested in improving yourself, then you’re the person we’re looking fo",
+      "Every live match comes with its own set of online statistics. Dive into the data, analyze it, and craft your winning strategy to predict the next match winner. You will find real-time predictions and their values from other players, Number and value of boosters for each option, Win rates based on previous matches and Additional valuable insights to enhance your strategy.",
     to: "/Who-can-join-FTMO?",
   },
   {
     id: 3,
-    question: "How to start?",
+    question: "How can I trust game rules and transactions?",
     answer:
-      "After you sign up, you can either start an FTMO Challenge right away or, as we usually recommend, start with our Free Trial, which is a free version of our FTMO Challenge intended for practicing purposes. In the Free Trial, you will also have the possibility to test some of our Trading Applications and you will also receive a shortened analysis version of your performance in the Free Trial. You can retake your Free Trial as many times as you want to make sure you are fully prepared for an FTMO Challenge",
+      "Each game match comes with a unique wallet address, ensuring all transactions—predictions and rewards—are visible to everyone. Check game reports and the blockchain anytime to see every detail clearly. key features are, unique wallet address for each match, transparent transactions visible to all players and real-time tracking on the blockchain. So stay informed and confident with every prediction you make!",
     to: "/How-to-start?",
   },
   {
     id: 4,
-    question: "Who can join FTMO?",
+    question: "What is benefit of inviting my friends to this game?",
     answer:
-      "No specific qualifications are required. All clients must be over the age of 18. Whether you are a beginner or already know your way around trading and are interested in improving yourself, then you’re the person we’re looking fo",
+      "Invite your friends to join Tricksfor Gaming Hub and reap the rewards! Here’s how it works: Match Rewards: Earn 1% of your friend’s reward value every time they win a match. NFT Purchases: Get 10% of the trade value whenever they buy any NFT collection.",
     to: "/Who-can-join-FTMO?",
   },
   {
     id: 5,
-    question: "How to start?",
+    question: "What is streak?",
     answer:
-      "After you sign up, you can either start an FTMO Challenge right away or, as we usually recommend, start with our Free Trial, which is a free version of our FTMO Challenge intended for practicing purposes. In the Free Trial, you will also have the possibility to test some of our Trading Applications and you will also receive a shortened analysis version of your performance in the Free Trial. You can retake your Free Trial as many times as you want to make sure you are fully prepared for an FTMO Challenge",
+      "A streak is the number of consecutive daily predictions you make. Keep your streak alive by placing a prediction in each game every day. Stay consistent, watch your streak grow, and earn extra rewards at specific milestones according to each game’s rules. Don’t miss out on these bonus opportunities!",
     to: "/How-to-start?",
   },
   {
     id: 6,
-    question: "Who can join FTMO?",
+    question: "What is booster?",
     answer:
-      "No specific qualifications are required. All clients must be over the age of 18. Whether you are a beginner or already know your way around trading and are interested in improving yourself, then you’re the person we’re looking fo",
+      "Boosters are staked NFTs from Tricksfor collections. Purchase as many as you like from NFT marketplaces like OpenSea and stake them in the app. Each NFT can enhance your reward if your prediction wins. Check the metadata of each NFT collection to find the boost rate.",
     to: "/Who-can-join-FTMO?",
   },
 ];
